846b279b1986a86f2ae82d5982ea349360c9e87d
dacup72 <dacup72@gmail.com>
2018-05-22 17:29:04 -0600

    Completed reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,31 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let intArray = n.toString().split("");
+  let negative = false;
+  let reverseIntString = "";
+
+  if(n === 0 || n === -0){
+    return n;
+  }
+
+  for (let index = 0; index < intArray.length; index++) {
+    if(reverseIntString === "" && intArray[index] === "0") {
+      null;
+    } else if(intArray[index] === "-") {
+      negative = true;
+    } else {
+      reverseIntString = intArray[index] + reverseIntString;
+    }
+  }
+
+  if(negative) {
+    reverseIntString = "-" + reverseIntString;
+    return parseInt(reverseIntString);
+  } else {
+    return parseInt(reverseIntString);
+  }
+}
 
 module.exports = reverseInt;
