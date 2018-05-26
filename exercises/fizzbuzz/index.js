ffc291bdb8e22ea111210e7f1011a68fa3887187
dacup72 <dacup72@gmail.com>
2018-05-25 20:01:54 -0600

    Played with the code

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -1,16 +1,17 @@
-// --- Directions
-// Write a program that console logs the numbers
-// from 1 to n. But for multiples of three print
-// “fizz” instead of the number and for the multiples
-// of five print “buzz”. For numbers which are multiples
-// of both three and five print “fizzbuzz”.
-// --- Example
-//   fizzBuzz(5);
-//   1
-//   2
-//   fizz
-//   4
-//   buzz
+// // --- Directions
+// // Write a program that console logs the numbers
+// // from 1 to n. But for multiples of three print
+// // “fizz” instead of the number and for the multiples
+// // of five print “buzz”. For numbers which are multiples
+// // of both three and five print “fizzbuzz”.
+// // --- Example
+// //   fizzBuzz(5);
+// //   1
+// //   2
+// //   fizz
+// //   4
+// //   buzz
+
 
 function fizzBuzz(n) {
   for (let index = 1; index <= n; index++) {
@@ -26,4 +27,15 @@ function fizzBuzz(n) {
   }
 }
 
-module.exports = fizzBuzz;
+
+// module.exports = fizzBuzz;
+
+
+
+
+
+
+
+
+
+
