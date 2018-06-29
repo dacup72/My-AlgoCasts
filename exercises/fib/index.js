857b90418e57d7932ef227a87be8090e522cfa52
dacup72 <dacup72@gmail.com>
2018-06-29 11:54:46 -0600

    Complete 2 exercises

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -21,17 +21,29 @@
 // }
 
 
-function fib(n) {
+function slowFib(n) {
   if(n < 2) {
     return n;
   }
-
-
   return fib(n - 1) + fib(n - 2);
- 
-  
 }
 
+function memoize(fn) {
+  let cache = {};
+  return function(...args) {
+    
+    if(cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+const fib = memoize(slowFib);
 
 
 module.exports = fib;
--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,29 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [
+
+    ]
+  }
+
+  add(input) {
+    this.data.unshift(input);
+  }
+
+  remove() {
+    return this.data.pop();
+    
+  }
+}
+
+// const q = new Queue;
+// q.add(1);
+// q.add(2);
+// q.add(3);
+// console.log(q.data)
+// console.log(q.remove());
+// console.log(q.data)
 
 module.exports = Queue;
