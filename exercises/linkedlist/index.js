8cb46c27cacdad957a8c1d0d1f20084ff3766c4c
dacup72 <dacup72@gmail.com>
2018-08-30 14:24:46 -0600

    working on insert at method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -121,6 +121,23 @@ class LinkedList {
 
     prevNode.next = prevNode.next.next;
   }
+
+  insertAt(data, index) {
+    if(!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if(index === 0) {
+      this.head = new Node(data, this.head);
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast;
+    const node = new Node(data, previous.next);
+    previous.next = node;
+
+    
+  }
 }
 
 module.exports = { Node, LinkedList };
